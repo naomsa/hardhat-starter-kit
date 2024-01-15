43c99d8365d7e54cee46e37b182de57540d1bdce
naomsa <djbrucelee@protonmail.com>
2024-01-15 13:05:45 -0300

    feat: added hardhat-network-helpers

--- test/index.ts
+++ test/index.ts
@@ -1,22 +1,27 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 
 describe("Greeter", () => {
-  it("Should return the new greeting once it's changed", async () => {
+  async function deployContracts() {
     const contract = await (
       await ethers.getContractFactory("Greeter")
     ).deploy("Hello, world!");
+
+    return contract;
+  }
+
+  it("Should return the new greeting once it's changed", async () => {
+    const contract = await loadFixture(deployContracts);
 
     expect(await contract.greeting()).to.equal("Hello, world!");
 
     await contract.setGreeting("Hola, mundo!");
-    expect(await contract.greeting()).to.equal("Hola, mundo!");
+    expect(await contract.greeting()).to.equal("Hello, world!");
   });
 
   it("Should emit the Greet event once greet() is called", async () => {
-    const contract = await (
-      await ethers.getContractFactory("Greeter")
-    ).deploy("Hello, world!");
+    const contract = await loadFixture(deployContracts);
 
     expect(await contract.greet()).to.emit(contract, "Greet");
   });
