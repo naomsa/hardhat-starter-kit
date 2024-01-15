4aee077d9b6b73eef7ab1d95dbf149843ce40797
naomsa <djbrucelee@protonmail.com>
2024-01-15 13:05:43 -0300

    refactor: updated tasks and removed scripts

--- tasks/accounts.ts
+++ tasks/accounts.ts
@@ -1,9 +1,8 @@
-import { Signer } from "@ethersproject/abstract-signer";
 import { task } from "hardhat/config";
+import type { Signer } from "@ethersproject/abstract-signer";
 
-task("accounts", "Prints the list of accounts", async (_taskArgs, hre) => {
-  const accounts: Signer[] = await hre.ethers.getSigners();
-
+task("accounts", "Prints the list of accounts", async (_, { ethers }) => {
+  const accounts: Signer[] = await ethers.getSigners();
   for (const account of accounts) {
     console.log(await account.getAddress());
   }
--- tasks/deploy/greeter.ts
+++ tasks/deploy/greeter.ts
@@ -4,11 +4,11 @@ import type { TaskArguments } from "hardhat/types";
 import { Greeter } from "../../typechain";
 
 task("deploy:greeter")
-  .addParam("greeting", "Say hello, be nice")
-  .setAction(async function (taskArguments: TaskArguments, { ethers }) {
+  .addPositionalParam("greeting", "Say hello, be nice")
+  .setAction(async function ({ greeting }: TaskArguments, { ethers }) {
     const contract: Greeter = await (
       await ethers.getContractFactory("Greeter")
-    ).deploy(taskArguments.greeting);
+    ).deploy(greeting);
 
     await contract.deployed();
     console.log("Greeter deployed to: ", contract.address);
